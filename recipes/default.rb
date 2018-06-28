45120881d0ecf314138270a189352414ebb9aa85
Mandi Walls <mandi.walls@gmail.com>
2018-06-28 08:59:40 +0100

    Removing old school inspec profile

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,26 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
-remote_file '/tmp/inspec-1.23.0-1.el7.x86_64.rpm' do
-  source "https://packages.chef.io/files/stable/inspec/1.23.0/el/7/inspec-1.23.0-1.el7.x86_64.rpm"
+remote_file '/tmp/inspec.rpm' do
+  source "https://packages.chef.io/files/stable/inspec/2.2.20/el/7/inspec-2.2.20-1.el7.x86_64.rpm"
 end
 
 package "inspec" do
-  #source "https://packages.chef.io/files/stable/inspec/1.23.0/el/7/inspec-1.23.0-1.el7.x86_64.rpm"
-  source "/tmp/inspec-1.23.0-1.el7.x86_64.rpm"
+  source "/tmp/inspec.rpm"
   action :install
 end
 
 package 'git' 
 
-git '/home/vagrant/linux-baseline' do
-  repository 'https://github.com/dev-sec/linux-baseline.git'
-  revision 'master'
-  action :sync
-end
-
-package 'rng-tools'
-
-service 'rngd' do
-  action [:start,:enable]
-end
